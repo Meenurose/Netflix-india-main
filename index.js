0a7d2ad0ab13100bba4a12540dfa3e8b8e2bc4de
Meenurose <meenurose20@gmail.com>
2023-04-21 09:36:38 +0530

    first modification

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// what for each do is takes one value from
-// array at a time and assigns it( the value, its' index as secod parameter and whole its array as the thord parameter)
-// to a function as a parameter and passing an argument gets us the access of the value
-
 faqs = document.querySelectorAll(".faq");
 faqs.forEach((name, index, array) => {
   name.addEventListener("click", () => {
@@ -15,6 +11,4 @@ faqs.forEach((name, index, array) => {
 
 });
 })
-// name argument in for-each loop is like this that returns the exact value that is selected of an array
-//next() in jquery returns the next element is the same DOM tree
-// when using display none for accordion do not use display: block in another class of same element as it will override
+
